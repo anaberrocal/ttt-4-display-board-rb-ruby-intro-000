b59fcdd4796e63eefa4dab15d8f982c263e5a10e
Ana Berrocal <anaberrocal@ymail.com>
2020-02-09 07:52:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-    #{board[0]}  |  #{board[1]} |  #{board[0]}
+  puts "
+    #{board[0]}  |  #{board[1]} |  #{board[2]}
    -----------
-  #{board[0]}    |  #{board[0]} |  #{board[0]}
+    #{board[3]}  |  #{board[4]} |  #{board[5]}
    -----------
-  #{board[0]}    |  #{board[0]} |  #{board[0]}  
+    #{board[6]}  |  #{board[7]} |  #{board[8]}  "
 end
